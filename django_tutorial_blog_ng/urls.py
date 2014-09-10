602a51fd6e0552a7f52d026856f95ade220402aa
Joshua Willett <jwillet1@gmail.com>
2014-09-10 17:25:10 -0400

    Implemented list view for posts

--- django_tutorial_blog_ng/urls.py
+++ django_tutorial_blog_ng/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # blog url's
+    url(r'^.*$', include('blogengine.urls'))
 )
