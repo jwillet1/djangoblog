f94422716baf5a86269c291363de5fc0346c0dd2
Joshua Willett <jwillet1@gmail.com>
2014-09-10 10:13:35 -0400

    Added tests for admin auth

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
 from blogengine.models import Post
 
@@ -8,21 +8,21 @@ class PostTest(TestCase):
 		# create the Post
 		post = Post()
 
-		#set attributes
+		# set attributes
 		post.title = 'My first post'
 		post.text = 'This is my first blog post'
 		post.pub_date = timezone.now()
 
-		#save it
+		# save it
 		post.save()
 
-		#check that we can find it
+		# check that we can find it
 		all_posts = Post.objects.all()
 		self.assertEquals(len(all_posts), 1)
 		only_post = all_posts[0]
 		self.assertEquals(only_post, post)
-		
-		#check attribures
+
+		# check attribures
 		self.assertEquals(only_post.title, 'My first post') 
 		self.assertEquals(only_post.text, 'This is my first blog post')
 		self.assertEquals(only_post.pub_date.day, post.pub_date.day)
@@ -31,3 +31,53 @@ class PostTest(TestCase):
 		self.assertEquals(only_post.pub_date.hour, post.pub_date.hour)
 		self.assertEquals(only_post.pub_date.minute, post.pub_date.minute)
 		self.assertEquals(only_post.pub_date.second, post.pub_date.second)
+
+class AdminTest(LiveServerTestCase):
+	fixtures = ['users.json']
+
+	def test_login(self):
+		# create Client
+		c = Client()
+
+		# get login page
+		response = c.get('/admin/login/?next=/admin/')
+
+		# check response code
+		self.assertEquals(response.status_code, 200)
+
+		# check 'Log in' in response
+		self.assertTrue('Log in' in response.content)
+
+		# log the user in
+		c.login(username='bobsmith', password="password")
+
+		# check response code
+		response = c.get('/admin/')
+		self.assertEquals(response.status_code, 200)
+
+		# check 'Log out' in response
+		self.assertTrue('Log out' in response.content)
+
+	def test_logout(self):
+		# create Client
+		c = Client()
+
+		# log in
+		c.login(username='bobsmith', password="password")
+
+		# check response code
+		response = c.get('/admin/')
+		self.assertEquals(response.status_code, 200)
+
+		# check 'Log out' in response
+		self.assertTrue('Log out' in response.content)
+
+		# log out
+		c.logout()
+
+		# get login page
+		response = c.get('/admin/login/?next=/admin/')
+		self.assertEquals(response.status_code, 200)
+
+		# check 'Log in' in response
+		self.assertTrue('Log in' in response.content)
