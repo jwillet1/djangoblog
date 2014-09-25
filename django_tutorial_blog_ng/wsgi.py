25e702e7f22c436e29f92452f7a0b37a69aa37ec
Joshua Willett <jwillet1@gmail.com>
2014-09-25 14:09:16 +0000

    configured app for heroku

--- django_tutorial_blog_ng/wsgi.py
+++ django_tutorial_blog_ng/wsgi.py
@@ -12,3 +12,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tutorial_blog_ng.setting
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
